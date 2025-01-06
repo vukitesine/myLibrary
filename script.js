149da659451eb9ded4fd5312cca8addd97d3c17f
vukitesine <139747602+vukitesine@users.noreply.github.com>
2025-01-06 23:31:21 +0100

    Step 2 and 3 done
    Created a constructor function and a function which adds newly created books to the library array.

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// create an array where all book objects will be stored
 myLibrary = [];
+
+function Book(title, author, page) {
+    this.title = title;
+    this.author = author;
+    this.page = page;
+};
+
+function addToLibrary(title, author, page) {
+    const newBook = new Book(title, author, page)
+    myLibrary.push(newBook);
+}
+
+addToLibrary('test', 'test', 23);
+console.log(myLibrary)
